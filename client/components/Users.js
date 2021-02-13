8cceac65ed9f0b566dd6b423c0a06e073a5d019f
Jason Williams <jwilliamstap@gmail.com>
2021-02-13 10:07:18 -0500

    Cleared merge conflicts. Some duplicate code needs to be filtered/adjusted

--- client/components/Users.js
+++ client/components/Users.js
@@ -2,10 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { fetchUsers } from "../store/users";
 import { Link } from "react-router-dom";
-<<<<<<< HEAD
 
-=======
->>>>>>> 3c168ec... Setup view/option switch per admin authorization
 
 export class Users extends React.Component {
   componentDidMount() {
@@ -22,23 +19,10 @@ export class Users extends React.Component {
             users.map((user) => {
               return (
                 <div key={user.id}>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 7609e2e... Added SingleUser component for admin to view individual user
                   <p>User Name: {<Link to={`/users/${user.id}`}> { user.name }</Link>}</p>
                   <p>User Email: { user.email }</p>
                   <p>User Github ID: { user.githubId ? user.githubId : 'No linked Github account.'}</p>
                   <p>Admin? { user.adminAuth ? 'Yes' : 'No' }</p>
-<<<<<<< HEAD
-=======
-                    <p>User Name: {<Link to={`/users/${user.id}`}> { user.name }</Link>}</p>
-                    <p>User Email: { user.email }</p>
-                    <p>User Github ID: { user.githubId ? user.githubId : 'No linked Github account.'}</p>
-                    <p>Admin? { user.adminAuth ? 'Yes' : 'No' }</p>
->>>>>>> 3c168ec... Setup view/option switch per admin authorization
-=======
->>>>>>> 7609e2e... Added SingleUser component for admin to view individual user
                 </div>
               );
             })
