749e982b5c95bb4b7a346180e189239d90d9cdbe
Kayla <frankumk@gmail.com>
2021-02-15 18:19:53 -0800

    Update cart: history.push not a func

--- client/components/index.js
+++ client/components/index.js
@@ -3,11 +3,12 @@
  * This way, we can import all of our components from the same place, rather than
  * having to figure out which file they belong to!
  */
+
 export {default as Navbar} from './navbar'
 export {default as Home} from './home'
 export {Login, Signup} from './auth-form'
 export {default as AllBooks} from './AllBooks'
-export {default as Cart} from './Cart'
+// export {default as Cart} from './Cart'
 export {default as SingleBook} from './SingleBook'
 export {default as Users} from './Users'
 export {default as AddBook} from './AddBook'
