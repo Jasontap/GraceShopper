a5396e428cc930e33d9db61d193d32bf911afd07
Kayla <frankumk@gmail.com>
2021-02-17 23:12:59 -0800

    Guest total working at last

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -14,10 +14,19 @@ export class Cart extends React.Component{
     this.removeFromGuestCart = this.removeFromGuestCart.bind(this)
     this.updateGuestCart = this.updateGuestCart.bind(this)
   }
+  stackCart(localcart){
+    let cart = []
+    let total = 0
+    for(let key in localcart){
+      cart.push({book: key, quantity: localcart[key].quantity, price: localcart[key].price})
+      total += localcart[key].quantity * localcart[key].price;
+    }
+    return {cart,total}
+  }
   componentDidMount(){
     const userId = this.props.auth.id;
     let cart;
-    let total;
+    let total=0;
     if(userId){
       cart = this.props.usercart
       total = cart.reduce((accum,item)=>{
@@ -28,16 +37,17 @@ export class Cart extends React.Component{
       cart = [];
       const localcart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : {};
       for(let key in localcart){
-        cart.push({book: key, quantity: localcart[key]})
+        cart.push({book: key, quantity: localcart[key].quantity, price: localcart[key].price})
+        total += localcart[key].quantity * localcart[key].price;
       }
     }
     this.setState({cart, total})
   }
-  componentDidUpdate(prevProps){
-    if(prevProps.usercart.length !== this.props.usercart.length){
+  componentDidUpdate(prevProps, prevState){
+    if(prevProps.usercart.length !== this.props.usercart.length || prevState.total !== this.state.total){
       const userId = this.props.auth.id;
       let cart;
-      let total;
+      let total=0;
       if(userId){
         cart = this.props.usercart
         total = cart.reduce((accum,item)=>{
@@ -48,7 +58,8 @@ export class Cart extends React.Component{
         cart = [];
         const localcart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : {};
         for(let key in localcart){
-          cart.push({book: key, quantity: localcart[key]})
+          cart.push({book: key, quantity: localcart[key].quantity, price: localcart[key].price})
+          total += localcart[key].quantity * localcart[key].price;
         }
       }
       this.setState({cart, total})
@@ -61,28 +72,32 @@ export class Cart extends React.Component{
     localStorage.setItem('cart', JSON.stringify(localcart));
     let cart = [];
     for(let key in localcart){
-      cart.push({book: key, quantity: localcart[key]})
+      cart.push({book: key, quantity: localcart[key].quantity, price: localcart[key].price})
     }
-    this.setState({cart})
+    const total = this.state.total - book.price
+    this.setState({cart, total})
   }
   updateGuestCart(book,qty){
+    console.log(`quantity: ` + qty)
     if(qty===0){
       this.removeFromGuestCart(book)
       return
     }
     let localcart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : {};
     let title = book.book;
-    localcart[title] = qty
+    localcart[title].quantity = qty
     localStorage.setItem('cart', JSON.stringify(localcart));
     let cart = [];
+    let total = 0;
     for(let key in localcart){
-      cart.push({book: key, quantity: localcart[key]})
+      cart.push({book: key, quantity: localcart[key].quantity, price: localcart[key].price})
+      total += localcart[key].quantity * localcart[key].price;
     }
-    this.setState({cart})
+    this.setState({cart,total})
   }
   clearCart(){
     localStorage.removeItem('cart');
-    this.setState({cart: []});
+    this.setState({cart: [], total: 0});
   }
   render(){
     const cart = this.state.cart
